15a857e84a10875470c1b2df956c67eb94af3e77
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-11-28 16:13:48 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+		<style>
+      body{
+	background: #ffffff;
+	position: relative;
+  margin: 100px 45%;
+}
+.ice-cream {
+	position: relative;
+	width: 80px;
+}
+.cream{
+	width: 80px;
+	height: 150px;
+	position: relative;
+}
+.ice-cream-1,
+.ice-cream-2,
+.ice-cream-3{
+	width: 80px;
+	height: 50px;
+}
+.ice-cream-1{
+	background: #f3ebbd;
+	border-top-right-radius: 25px;
+	border-top-left-radius: 25px;
+}
+.ice-cream-2{
+	background: #65bd96;
+}
+.ice-cream-3{
+	background: #e56040;
+	-webkit-border-bottom-right-radius: 3px;
+  -moz-border-bottom-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+	-webkit-border-bottom-left-radius: 3px;
+  -moz-border-bottom-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+.lute {
+	position: absolute;
+	height: 175px;
+	width: 10px;
+	background: #f0bc3b;
+	left: 35px;
+	top: 30px;
+	z-index: -999;
+	-webkit-border-radius: 99px;
+  -moz-border-radius: 99px;
+  border-radius: 99px;
+}
+      </style>
+	</head>
+  <body>
+		<table>
+		<th>
+      		  <div class="ice-cream">
+			  <div class="cream">
+				  <div class="ice-cream-1"></div>
+				  <div class="ice-cream-2"></div>
+				  <div class="ice-cream-3"></div>
+			  </div>
+			  <div class="lute"></div>
+		  </div>
